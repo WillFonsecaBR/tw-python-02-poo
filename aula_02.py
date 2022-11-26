42ba2c41f8ffa45e16201f9e1fcda571164140ca
Willian Fonseca <willian.alves2109@gmail.com>
2022-11-26 07:38:55 -0300

    Finalizando aula 2

--- aula_02.py
+++ aula_02.py
@@ -1,1 +1,42 @@
 # AULA 0 - CLASSES E OBJETOS
+
+### Criando uma nova classe
+class carro():
+    """DOCSTRING: a DOCSTRING É UTILIZADA PARA DESCREVER CLASSES E METODOS FACILITANDO O ENTENDIMENTO DOS MESMOS"""
+    """EXEMPLO DE DOCSTRING GERAL"""
+
+    # Atributos da classe
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado):
+        """DOCSTRING: Exemplo de docstring dos atributos da classe"""
+        self.cor = cor;
+        self.qtd_portas = qtd_portas
+        self.tipo_combustivel = tipo_combustivel
+        self.potencia = potencia
+        self.qtd_combustivel = qtd_combustivel
+        self.is_ligado = is_ligado
+
+    ## Metodos da classe
+    def abastecer(self):
+        """DOCSTRING: Exemplo de docstring da classe abastecer"""
+        self.qtd_combustivel += 20
+
+    def ligar(self):
+        """DOCSTRING: Exemplo de docstring da classe ligar"""
+        if self.is_ligado == True:
+            print("O carro já esta ligado!")
+        else:
+            self.is_ligado = True
+
+    def desligar(self):
+        """DOCSTRING: Exemplo de docstring da classe desligar"""
+        self.is_ligado = False
+
+
+"""
+Para recorrer a docstring de qualquer metodo ou classe basta chamar o metodo ou classe
+utilizando o atributo help
+
+help(INSTANCIA.CLASSE/METODO) 
+
+Depois é só rodar o codigo
+"""
