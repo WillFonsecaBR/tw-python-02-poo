fd83963fd0a23cf868cd8b55f25f059ce043c3cd
Willian Fonseca <willian.alves2109@gmail.com>
2022-11-25 21:16:10 -0300

    Finalizando aula 1

--- aula_01.py
+++ aula_01.py
@@ -1,1 +1,23 @@
 # AULA 01 - ESCOPOS E NAMESPACES
+
+## Importando namespaces
+
+# from nome_do_modulo import * => Importa tudo
+
+# from nome_do_modulo import metodo_a, metodo_b => Importa somente os modulos escolhidos
+
+# import nome_do_modulo => Mais utilizada, você deve usar o nome da biblioteca.
+
+# CTRL + BACKSPACE => mostra as funções dentros dos modulos
+
+
+import math # MODO CORRETO DE IMPORTAR
+
+print(math.isqrt(20)) # chamando uma função do modulo
+
+## Resolução de escopo
+
+nome = "João" #escopo global
+
+def funcao():
+  nome = "José" #escopo local
